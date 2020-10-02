519e8e25621aa6a3556106520a1cf3c63e909412
April Darger <april.m.darger@gmail.com>
2020-10-02 10:48:52 -0600

    In Cards.js set hierarchy and attributes/text.

--- components/Cards.js
+++ components/Cards.js
@@ -25,7 +25,7 @@ function articleCardMaker(article) {
     axios.get('https://lambda-times-api.herokuapp.com/articles')
         .then(res => {
 
-            //instantize elements
+            // instantize elements
             const card = document.createElement('div')
             const headline = document.createElement('div')
             const author = document.createElement('div')
@@ -33,10 +33,36 @@ function articleCardMaker(article) {
             const image = document.createElement('img')
             const nameAuthor = document.createElement('span')
 
-            card.classList()
+            // add class names
+            card.classList.add('card')
+            headline.classList.add('headline')
+            author.classList.add('author')
+            imgContainer.classList.add('img-container')
+
+            // set hierarchy
+            card.appendChild(headline)
+            card.appendChild(author)
+            author.appendChild(imgContainer)
+            author.appendChild(nameAuthor)
+            imgContainer.appendChild(image)
+
+            // attributes and text
+            headline.textContent = res.data.articles.bootstrap.headline
+            image.src = res.data.articles.bootstrap.authorPhoto
+            nameAuthor.textContent = `By ${res.data.articles.bootstrap.authorName}`
 
         })
         .catch(err => {
             debugger
         })
 }
+
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {author's name}</span>
+//   </div>
+// </div>
